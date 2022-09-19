42db8c4b1ae98704e6b3f2ebe13c1dc59ca01845
eduardo <rivera.eduardo@berkeley.edu>
2022-09-19 15:09:22 -0700

    Updated deploy-dev.sh script to ca-dev values

--- demos/serverless/deploy-dev.sh
+++ demos/serverless/deploy-dev.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 npm install
-npm run deploy -- -r us-west-2 \
--b onto-chime-video-deployment \
--s ChimeStack \
--cr us-west-2 \
--m https://meetings-chime.us-west-2.amazonaws.com \
---chime-sdk-media-pipelines-endpoint https://media-pipelines-chime.us-west-2.amazonaws.com \
---chime-sdk-media-pipelines-region us-west-2 \
---profile ontometrics2
+npm run deploy -- -r us-east-1 \
+-b ehawk-chime-demo2 \
+-s ehawk-chime-stack2 \
+-cr us-east-1 \
+-m https://meetings-chime.us-east-1.amazonaws.com \
+--chime-sdk-media-pipelines-endpoint https://media-pipelines-chime.us-east-1.amazonaws.com \
+--chime-sdk-media-pipelines-region us-east-1 \
+--profile ehawk-prod
