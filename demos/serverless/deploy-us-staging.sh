40ea1bbd8d13e7eec56507c8f080ded8e6e116b8
eduardo <rivera.eduardo@berkeley.edu>
2022-10-06 13:41:37 -0700

    Added deploy-us-staging.sh script

---
+++ demos/serverless/deploy-us-staging.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+npm install
+npm run deploy -- -r us-east-2 \
+-b ehawk-chime-us-staging \
+-s ehawk-chime \
+-cr us-east-1 \
+-m https://meetings-chime.us-east-1.amazonaws.com \
+--chime-sdk-media-pipelines-endpoint https://media-pipelines-chime.us-east-1.amazonaws.com \
+--chime-sdk-media-pipelines-region us-east-1 \
+--profile ehawk-prod
